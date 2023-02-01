d325d98e3aadf3b190cfa430ddc722540c921d52
Gilbert189 <gilbertdannellelo@gmai;.com>
2023-02-01 22:44:32 +0700

    Added profile-pics on tampermonkey

---
+++ tampermonkey/profile-pics/profile-pics.user.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name         Profile Pictures
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  This adds profile pictures to posts.
+// @author       Gilbert189
+// @match        https://tbgforums.com/forums/viewtopic.php?*
+// @icon         https://tbgforums.com/favicon.ico
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    console.log("ProfilePicture loaded");
+    var main = document.getElementById("brdmain");
+    var posts = main.getElementsByClassName("blockpost");
+    for (var post of posts) {
+        var signature = post.getElementsByClassName("postsignature")[0].innerHTML;
+        var table = post.getElementsByClassName("postleft")[0].children[0];
+        let match = /\[avatar=(.+?)\]/.exec(signature);
+        if (match !== null && table.getElementsByClassName("postavatar").length == 0) {
+            var image = document.createElement("img");
+            image.src = encodeURI(match[1]);
+            image.width = 90; image.height = 90;
+            image.className = "postavatar";
+            var after = table.getElementsByClassName("usertitle")[0];
+            table.insertBefore(image, after.nextSibling);
+        }
+}
+})();
